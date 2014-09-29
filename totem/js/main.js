bb1c5d2721dbd3092c1455b78d9e9720a9f390d7
ericholiveira <ericholiveira10@gmail.com>
2014-09-29 11:41:02 -0300

    programacao totem

--- totem/js/main.js
+++ totem/js/main.js
@@ -1,88 +1,78 @@
-function errorHandler(jqXHR, exception) {
-    if (jqXHR.status === 0) {
-        alert('Not connect.\n Verify Network.');
-    } else if (jqXHR.status == 404) {
-        alert('Requested page not found. [404]');
-    } else if (jqXHR.status == 500) {
-        alert('Internal Server Error [500].');
-    } else if (exception === 'parsererror') {
-        alert('Requested JSON parse failed.');
-    } else if (exception === 'timeout') {
-        alert('Time out error.');
-    } else if (exception === 'abort') {
-        alert('Ajax request aborted.');
-    } else {
-        alert('Uncaught Error.\n' + jqXHR.responseText);
+var blockPress = false;
+
+function submit(value) {
+  var anvisaNumber, serialNumber;
+  blockPress = true;
+  this.$("#enterDataMatrix").val('');
+  value = dataMatrix.parseDataMatrix(value);
+  if (!this.validateRBParsedDataMatrix(value)) {
+    anvisaNumber = parsedDataMatrix["713"];
+    serialNumber = parsedDataMatrix["21"];
+    $.ajax({
+      type: "GET",
+      url: "http://dashboard.rastreabilidadebrasil.com.br/rest/1/report/medical_item/public/anvisa/" +
+        anvisaNumber + "/serial/" + serialNumber,
+      dataType: 'json',
+      success: function(data) {
+        var idToShow=3;//Error
+        if(+data.status!==0){
+          idToShow=2;
+        }
+        $(".et1").animate({
+          opacity: 0.0
+        }, 500, function() {
+          showEt(idToShow);
+        });
+
+      },
+      error: function(error){
+          if(+error.status===404){
+            $(".et1").animate({
+              opacity: 0.0
+            }, 500, function() {
+              showEt(4);
+            });
+          }
+      }
+    });
+  }
+}
+
+function checkValidade() {
+  var keyCode = e.keyCode || e.which;
+  if (keyCode === dataMatrix.DEFAULT_ASCII_SEPARATOR_CODE) {
+    var newVal = this.$("#enterDataMatrix").val() + dataMatrix.DEFAULT_COMMON_SEPARATOR;
+    this.$("#enterDataMatrix").val(newVal);
+  } else {
+    if (keyCode === 13) {
+      submit(this.$("#enterDataMatrix").val());
     }
+  }
 }
 
-function checkValidade(value){
-	blockPress = true;
-	
-	/*$.ajax({
-	  type: "POST",
-	  url: String("_php/getcadastradas.php"),
-	  dataType: 'json',
-	  success: function(data){
-	  	
-	  	//data.cadastradas
-
-	  },
-	  error: errorHandler
-	});*/
-	if(value=="11"){
-		$( ".et1" ).animate({
-			opacity: 0.0
-		}, 500, function() {
-			showEt(2);
-		});
-	}else{
-		$( ".et1" ).animate({
-			opacity: 0.0
-		}, 500, function() {
-			showEt(4);
-		});
-	}
-}
-
-function showEt(id){
-	$( ".et"+id ).css("display","block");
-	$( ".et"+id ).css("opacity",0);
-	$( ".et"+id ).animate({
-		opacity: 1.0
-	}, 500, function() {
-		setTimeout(function(){
-			$( ".et"+id ).animate({
-				opacity: 0.0
-			}, 500, function() {
-				blockPress = false;
-				$( ".et"+id ).css("display","none");
-				$("textarea[maxlength]").val("");
-				$("textarea[maxlength]").focus();
-				$( ".et1" ).animate({
-					opacity: 1.0
-				}, 500);
-			});
-		},5000)
-	});
+function showEt(id) {
+  $(".et" + id).css("display", "block");
+  $(".et" + id).css("opacity", 0);
+  $(".et" + id).animate({
+    opacity: 1.0
+  }, 500, function() {
+    setTimeout(function() {
+      $(".et" + id).animate({
+        opacity: 0.0
+      }, 500, function() {
+        blockPress = false;
+        $(".et" + id).css("display", "none");
+        $("textarea[maxlength]").val("");
+        $("textarea[maxlength]").focus();
+        $(".et1").animate({
+          opacity: 1.0
+        }, 500);
+      });
+    }, 5000);
+  });
 }
 
 
-$(document).ready(function(){ 
-	$("textarea[maxlength]").focus();
-	$("textarea[maxlength]").keyup(function(event){
-		if(!blockPress){
-		    var key = event.which;
-	        var maxLength = $(this).attr("maxlength");
-	        var length = this.value.length;
-
-	        if(length == maxLength) {
-	        	checkValidade($(this).val());
-	            event.preventDefault();
-	        }
-    	}
-	});
+$(document).ready(function() {
+  $("textarea[maxlength]").focus();
 });
-
-
-var blockPress=false;
