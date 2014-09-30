a2310585d828fd1b21ee134f834fa76e9e4ba510
ericholiveira <ericholiveira10@gmail.com>
2014-09-30 14:17:27 -0300

    totem2

---
+++ totem2/js/main.js
@@ -0,0 +1,81 @@
+var blockPress = false;
+
+function submit(value) {
+  var anvisaNumber, serialNumber;
+  blockPress = true;
+  $("#enterDataMatrix").val('');
+  value = dataMatrix.parseDataMatrix(value);
+  anvisaNumber = value["713"];
+  serialNumber = value["21"];
+  $.ajax({
+    type: "GET",
+    url: "http://dashboard.rastreabilidadebrasil.com.br/rest/1/report/medical_item/public/anvisa/" +
+      anvisaNumber + "/serial/" + serialNumber,
+    dataType: 'json',
+    xhrFields: {
+      withCredentials: true
+    },
+    success: function(data) {
+      var idToShow = 3; //Error
+      if (+data.status !== 0) {
+        idToShow = 2;
+      }
+      $(".et1").animate({
+        opacity: 0.0
+      }, 500, function() {
+        showEt(idToShow);
+      });
+
+    },
+    error: function(error) {
+      if (+error.status === 404) {
+        $(".et1").animate({
+          opacity: 0.0
+        }, 500, function() {
+          showEt(4);
+        });
+      }
+    }
+  });
+
+}
+
+function checkValidade(e) {
+  var keyCode = e.keyCode || e.which;
+  e = e || window.event;
+  if (keyCode === dataMatrix.DEFAULT_ASCII_SEPARATOR_CODE) {
+    var newVal = $("#enterDataMatrix").val() + dataMatrix.DEFAULT_COMMON_SEPARATOR;
+    $("#enterDataMatrix").val(newVal);
+  } else {
+    if (keyCode === 13) {
+      submit($("#enterDataMatrix").val());
+    }
+  }
+}
+
+function showEt(id) {
+  $(".et" + id).css("display", "block");
+  $(".et" + id).css("opacity", 0);
+  $(".et" + id).animate({
+    opacity: 1.0
+  }, 500, function() {
+    setTimeout(function() {
+      $(".et" + id).animate({
+        opacity: 0.0
+      }, 500, function() {
+        blockPress = false;
+        $(".et" + id).css("display", "none");
+        $("textarea[maxlength]").val("");
+        $("textarea[maxlength]").focus();
+        $(".et1").animate({
+          opacity: 1.0
+        }, 500);
+      });
+    }, 5000);
+  });
+}
+
+
+$(document).ready(function() {
+  $("textarea[maxlength]").focus();
+});
