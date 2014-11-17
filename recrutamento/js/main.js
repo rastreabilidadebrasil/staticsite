d3cb54697e2f337c422d405bf34cda06e997b5a8
Thalles Freitas <thallesfreitas@yahoo.com.br>
2014-11-17 18:41:24 -0200

    [recrutamento]

--- recrutamento/js/main.js
+++ recrutamento/js/main.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
 	$(".thumb").click(function(){
-		
-		$(".img").html("<img src='imgs/img"+$(this).attr("value")+".jpg' />")
+		$(".img").append("<img style='display:none' src='imgs/img"+$(this).attr("value")+".jpg' />")
+		$(".img").fadeOut(400);
+		var img = $(this).attr("value");
+		setTimeout(function(){
+			$(".img").html("<img src='imgs/img"+img+".jpg' />")
+			setTimeout(function(){$(".img").fadeIn(400);},400);
+		},400);
 	})
 
 });
